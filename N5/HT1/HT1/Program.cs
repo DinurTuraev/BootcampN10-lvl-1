f93c005636b6eefb0c1dfbed6fe1aa6c03c5a222
Coderiy <dinurturaev@gmail.com>
2023-07-16 14:08:11 +0500

    done

---
+++ N5/HT1/HT1/Program.cs
@@ -0,0 +1,41 @@
+﻿string[] questions = {"O'zbekiston poytaxti?", "Amir Temur qachon tug'ilgan?"};
+
+string[] correct = {"Toshkent", "1336" };
+string[] wrong = { "Antananarivu", "1363" };
+int point = 0;
+
+Random rd = new Random();
+
+for (int i = 0; i < questions.Length; i++)
+{
+    Console.WriteLine(questions[i]);
+    if (rd.Next(0, 2) == 1)
+    {
+        Console.WriteLine($"A: {correct[i]}");
+        Console.WriteLine($"B: {wrong[i]}");
+        Console.WriteLine("Javob kiriting: A/B");
+        var answer = Console.ReadLine();
+        if (answer.Equals("A", StringComparison.OrdinalIgnoreCase))
+        {
+            point += 10;
+        }
+    }
+    else
+    {
+        Console.WriteLine($"A: {wrong[i]}");
+        Console.WriteLine($"B: {correct[i]}");
+        Console.WriteLine("Javob kiriting: A/B");
+        var answer = Console.ReadLine();
+        if(answer.Equals("B",StringComparison.OrdinalIgnoreCase))
+        {
+            point += 10;
+        }
+    }
+}
+
+Console.WriteLine($"Your score: {point}");
+
+
+
+
+
---
+++ N5/HT2/HT2/Program.cs
@@ -0,0 +1,39 @@
+﻿char command;
+do
+{
+    Console.WriteLine("Enter a number: ");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Enter command: ");
+    command = Convert.ToChar(Console.ReadLine());
+    
+    Console.WriteLine("Enter a second number: ");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+
+    if (command == '+')
+    {
+        Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+    }
+    else if (command == '-')
+    {
+        Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+    }
+    else if (command == '*')
+    {
+        Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+    }
+    else if (command == '/')
+    {
+        Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
+    }
+} while (command != 'e');
+
+
+
+
+
+
+
+
+
+
